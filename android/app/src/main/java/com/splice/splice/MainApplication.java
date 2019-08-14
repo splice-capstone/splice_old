0e51294092a39d19d6d0a9ccf3b950e8a4525fb0
tom.sinovich@gmail.com <tomsinovich@gmail.com>
2019-08-14 11:59:56 -0500

    initial ios and android

--- android/app/src/main/java/com/splice/splice/MainApplication.java
+++ android/app/src/main/java/com/splice/splice/MainApplication.java
@@ -1,4 +1,4 @@
-package com.invertase.rnfirebasestarter;
+package com.splice.splice;
 
 import android.support.multidex.MultiDexApplication;
 
--- ios/spliceTests/spliceTests.m
+++ ios/spliceTests/spliceTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RNFirebaseStarterTests : XCTestCase
+@interface spliceTests : XCTestCase
 
 @end
 
-@implementation RNFirebaseStarterTests
+@implementation spliceTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
